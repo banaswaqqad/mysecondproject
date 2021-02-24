e517963f3cc9a47019ff8a4d424437e7de0dd4f2
banaswaqqad <Banawaqqad7@gmail.com>
2021-02-24 20:34:04 +0200

    added the javascript lab

---
+++ lastday.js
@@ -0,0 +1,22 @@
+var age = 55; 
+
+while(entertheage != 55)
+{
+var entertheage = prompt("guess the age you think to enter the game");
+counter=0;
+alert("please try again")
+counter++
+}
+alert("congrats the correct number is " + age  );
+console.log( "congrats the correct number is " + age  );
+
+
+
+var numberofimg = prompt( "enter number of img you would show it " );
+var storenumberofimg = numberofimg;
+
+for (var i =0 ; i<storenumberofimg ; i++)
+    {
+
+        document.write('<img src="https://i.pinimg.com/originals/45/9c/90/459c9027f9f826354a1fac6ca9b5d714.jpg">');
+    }
